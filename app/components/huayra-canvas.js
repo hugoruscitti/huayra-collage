c0aa098b1cbe74f164b195a6e8169000a8914b92
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-05 20:30:27 -0300

    Mejorando layout general de la aplicación.

--- app/components/huayra-canvas.js
+++ app/components/huayra-canvas.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['huayra-canvas'],
+  width: 100,
+  height: 100,
+
   createCanvas: function() {
     var canvas = new fabric.Canvas('canvas');
 
@@ -11,6 +15,10 @@ export default Ember.Component.extend({
       height : 70,
       fill : 'red'
     });
+
+    canvas.setWidth(this.get("width"));
+    canvas.setHeight(this.get("height"));
+    canvas.renderAll();
 
     canvas.add(rect);
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    goToEditor: function() {
+      this.transitionTo("editor");
+    }
+  }
 });
